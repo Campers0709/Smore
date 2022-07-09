706d5598e31a93a124efef674060952092f1ca31
seitamuro <seitamuro0210@gmail.com>
2022-07-10 00:34:11 +0900

    nextjsのapiサーバーに/api/categoryのGETとPOSTを実装

--- next-app/pages/api/category.ts
+++ next-app/pages/api/category.ts
@@ -7,7 +7,17 @@ export default async function handler(
   res: NextApiResponse<any>
 ) {
   const API_URL = process.env.API_URL
-  console.log(API_URL)
-  const { data } = await axios.get(`${API_URL}test_v1/category`)
-  res.status(200).json(data)
+  const urlParams = new URLSearchParams(req.url)
+
+  if (req.method == 'GET') {
+    const { data } = await axios.get(
+      `${API_URL}test_v1/category?user_id=${urlParams.get('user_id')}`
+    )
+    res.status(200).json(data)
+  } else if (req.method == 'POST') {
+    const { data } = await axios.post(`${API_URL}text_v1/category`, {
+      user_id: urlParams.get('user_id'),
+    })
+    res.status(200).json(data)
+  }
 }
