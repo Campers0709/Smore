e7a5fb7d5d9fc7addcacd53cc5653989f2469131
masa5555 <masa57887@gmail.com>
2022-07-26 23:35:56 +0900

    URLのサムネイルが表示されるように変更

--- next-app/types/Article.ts
+++ next-app/types/Article.ts
@@ -2,9 +2,11 @@ export type Article = {
   item_id: string
   user_id: string
   category_id: number | null
-  url: string
   tweet: string
+  url: string
+  preview_image_url: string
   user_text: string
+  ai_summary: string
+  tweet_created_at: string
   liked_at: string
-  ai_summary?: string
 }
